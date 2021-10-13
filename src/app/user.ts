b77e639065d14359cd093e4baea86a926be1c886
Moringa <dancun.wawire@student.moringaschool.com>
2021-10-13 20:49:23 +0300

    this commit adds profile details

--- src/app/user.ts
+++ src/app/user.ts
@@ -3,10 +3,24 @@ export class User {
   login:String;
   bio:String;
   avatar_url:string
+  name:String;
+  company:String
+  followers:number
+  following:number
 
-  constructor(login:String, bio:String, avata:string){
+  constructor(login:String,
+    bio:String,
+     avata:string,
+     name:String,
+  company:String,
+  followers:number,
+  following:number,){
     this.bio = bio;
     this.login = login;
     this.avatar_url = avata;
+    this.name= name;
+    this.company=company;
+    this.followers=followers;
+    this.following=following;
   }
 }
