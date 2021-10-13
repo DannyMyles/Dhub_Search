29fbc302868ee6c77faadf74b258fb82159ebc6d
Moringa <dancun.wawire@student.moringaschool.com>
2021-10-13 20:57:57 +0300

    this commit adds clientis and secret

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,16 +1,5 @@
-// This file can be replaced during build by using the `fileReplacements` array.
-// `ng build` replaces `environment.ts` with `environment.prod.ts`.
-// The list of file replacements can be found in `angular.json`.
-
 export const environment = {
-  production: false
+  production: false,
+  clientid:'1279d7ec943e647e717e',
+  clientsecret:'1271fce86f453086ae073f7a83f4f5af7f95dca5'
 };
-
-/*
- * For easier debugging in development mode, you can import the following file
- * to ignore zone related error stack frames such as `zone.run`, `zoneDelegate.invokeTask`.
- *
- * This import should be commented out in production mode because it will have a negative impact
- * on performance if an error is thrown.
- */
-// import 'zone.js/plugins/zone-error';  // Included with Angular CLI.
--- src/services/profile.service.ts
+++ src/services/profile.service.ts
@@ -1,14 +1,15 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { map } from 'rxjs/operators'
+import { environment } from 'src/environments/environment';
 
 @Injectable({
   providedIn: 'root'
 })
 export class ProfileService {
   private username!: string;
-  private clientid = '1279d7ec943e647e717e'
-  private clientsecret = '1271fce86f453086ae073f7a83f4f5af7f95dca5'
+  private clientid = environment.clientid
+  private clientsecret = environment.clientsecret
 
   constructor(private http:HttpClient) {
   console.log("service is now ready");
@@ -18,4 +19,9 @@ export class ProfileService {
     return this.http.get("https://api.github.com/users/" + username +"?client_id"+ this.clientid + "&client_secret" + this.clientsecret)
     .toPromise();
   }
+  getRepositories(url:string){
+    return this.http.get( url +"?client_id"+ this.clientid + "&client_secret" + this.clientsecret)
+    .toPromise();
+  }
 }
+
