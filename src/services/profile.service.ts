c1015692e8c2710cd97345bd91ef3e8a01d86377
Moringa <dancun.wawire@student.moringaschool.com>
2021-10-13 16:50:15 +0300

    this commit adds clients secret

--- src/services/profile.service.ts
+++ src/services/profile.service.ts
@@ -10,7 +10,7 @@ import 'rxjs/add/operator/map';
 export class ProfileService {
   private username!: string;
   private clientid = '1279d7ec943e647e717e'
-  private clientsecret = ''
+  private clientsecret = '1271fce86f453086ae073f7a83f4f5af7f95dca5'
 
   constructor(private http:Http) {
   console.log("service is now ready");
